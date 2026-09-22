3510cc72b7adfb07ed4b8c14308f8868ca202d9c
maria helena <maria.h.silva100@aluno.senai.br>
2026-09-22 09:39:30 -0300

    src

--- src/src/server.js
+++ src/src/server.js
@@ -1,3 +1,4 @@
+const router = require('../routes/auth.routes');
 const autenticar = require('./src/middlewares/autenticar');
 
 app.use('/auth', authRoutes);
@@ -8,4 +9,15 @@ app.use('/tarefas', autenticar, tarefasRoutes);
 app.use('/usuarios', autenticar, usuarioRoutes);
 app.use('/projetos', autenticar, projetosRoutes);
 
-router.get('/')
+router.get('/', tarefasController.listar);
+router.get('/:id', tarefasController.buscarPorId);
+
+router.post('/',
+    autenticar,
+    validar(schemas.tarefa),
+    tarefasController.criar);
+
+    router.put('/:id', autenticar, tarefasController.atualizar);
+    router.delete('/:id', autenticar, tarefasController.remover);
+
+)
